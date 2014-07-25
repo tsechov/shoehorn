c2db89672b9e37e30d735f7e1ab3877dade27718
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-07-25 12:58:20 +0200

    cors cleanup

--- app/controllers/CorsAction.scala
+++ app/controllers/CorsAction.scala
@@ -3,40 +3,42 @@ package controllers
 import play.api.mvc.{SimpleResult, Results, Request, ActionBuilder}
 import scala.concurrent.Future
 import play.api.http.HeaderNames._
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit._
 
 
-object CorsAction extends ActionBuilder[Request] with Results{
-//  val MaxAge = 60 * 60 * 24 * 30
-val MaxAge = 1
-  val AllowHeaders = List(       "Content-Type",
-    "X-Auth-Token", "X-HTTP-Method-Override", "X-Json", "X-Prototype-Version", "X-Requested-With")
+object CorsAction extends ActionBuilder[Request] with Results {
+  val maxAge = Duration(30,DAYS).toSeconds.toString
 
-  val AllowMethods = List("POST", "GET", "PUT", "DELETE", "OPTIONS")
+  val allowHeaders = List("Content-Type",
+    "X-Auth-Token", "X-HTTP-Method-Override", "X-Json", "X-Prototype-Version", "X-Requested-With").mkString(", ")
 
-  val AllowCredentials = true
+  val allowMethods = List("POST", "GET", "PUT", "DELETE", "OPTIONS").mkString(", ")
 
-  def cors[A](origin: String) =
+  val allowCredentials = true.toString
+
+  def corsPreflight[A](origin: String) =
     Ok.withHeaders(
 
-      ACCESS_CONTROL_ALLOW_METHODS -> AllowMethods.mkString(", "),
-      ACCESS_CONTROL_ALLOW_HEADERS -> AllowHeaders.mkString(", "),
-      ACCESS_CONTROL_ALLOW_CREDENTIALS -> AllowCredentials.toString,
-      ACCESS_CONTROL_MAX_AGE -> MaxAge.toString,
-      ACCESS_CONTROL_ALLOW_ORIGIN -> origin,
-      "Access-Control-Expose-Headers" -> "Location")
+      ACCESS_CONTROL_ALLOW_METHODS -> allowMethods,
+      ACCESS_CONTROL_ALLOW_HEADERS -> allowHeaders,
+      ACCESS_CONTROL_ALLOW_CREDENTIALS -> allowCredentials,
+      ACCESS_CONTROL_MAX_AGE -> maxAge,
+      ACCESS_CONTROL_ALLOW_ORIGIN -> origin
+    )
 
   def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]) = {
     implicit val executionContext = play.api.libs.concurrent.Execution.defaultContext
     val origin = request.headers.get(ORIGIN).getOrElse("*")
 
     if (request.method == "OPTIONS") {
-      Future.successful(cors(origin))
+      Future.successful(corsPreflight(origin))
     } else {
       block(request).map(res =>
         res.withHeaders(
           ACCESS_CONTROL_ALLOW_ORIGIN -> origin,
-          ACCESS_CONTROL_ALLOW_CREDENTIALS -> AllowCredentials.toString,
-          "Access-Control-Expose-Headers" -> "Location"))
+          ACCESS_CONTROL_ALLOW_CREDENTIALS -> allowCredentials,
+          ACCESS_CONTROL_EXPOSE_HEADERS -> LOCATION))
     }
   }
 }
