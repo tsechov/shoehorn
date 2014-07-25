fa7fb22cee76cb0c01fa43263877ad125c5d15c8
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-07-25 13:40:34 +0200

    trying to get rid of doc.jar

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,5 @@
 import sbt._
+import sbt.Keys._
 
 object ApplicationBuild extends Build {
 
@@ -16,7 +17,9 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    sources in (Compile,doc) := Seq.empty
   )
+
+
 
 }
