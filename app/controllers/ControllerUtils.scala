43c4e09a06867d47d93b362fdd1e02ad1cb4a029
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-08-07 10:29:13 +0200

    404 -> [] if not found for find

--- app/controllers/ControllerUtils.scala
+++ app/controllers/ControllerUtils.scala
@@ -28,7 +28,7 @@ trait ControllerUtils {
 
   def toJsArray[A](list: List[A])(implicit w: Writes[A]): JsArray = list.foldLeft(JsArray())((acc, elem) => acc ++ Json.arr(elem))
 
-  def foundOrNot[A](resultList: List[A])(implicit w: Writes[A]) = if (resultList.isEmpty) NotFound else Ok(toJsArray(resultList)).as(ContentTypes.JSON)
+  def foundOrNot[A](resultList: List[A])(implicit w: Writes[A]) = Ok(toJsArray(resultList)).as(ContentTypes.JSON)
 
   def badQuery(query: String, error: Throwable) = {
     Logger.debug(s"error parsing query: $query")
