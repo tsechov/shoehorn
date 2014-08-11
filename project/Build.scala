9269b5d70c412f80f2b48098c5cb65090189f912
tsechov <tsechov@gmail.com>
2014-08-11 19:44:02 +0200

    adding current version object to source

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,8 @@ import sbt._
 import sbt.Keys._
 import play.Project._
 import sbtrelease.ReleasePlugin._
+import scala.Some
+import scala.Some
 
 object ApplicationBuild extends Build {
   releaseSettings
@@ -23,7 +25,12 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies, settings = Defaults.defaultSettings ++ playScalaSettings ++ releaseSettings).settings(
     sources in(Compile, doc) := Seq.empty,
     publishTo := Some(Resolver.file("file", new File(target.value.absolutePath + "/publish"))),
-    version := (version in ThisBuild).value
+    version := (version in ThisBuild).value,
+    sourceGenerators in Compile <+= Def.task {
+      val file = (sourceManaged in Compile).value / "release" / "CurrentVersion.scala"
+      IO.write(file, """package release; object CurrentVersion { def apply() = """" + (version in ThisBuild).value + """" }""")
+      Seq(file)
+    }
   )
 
 
