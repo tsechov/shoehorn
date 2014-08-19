45aff44a831b2dac6929774f56a1c1c28f5f5917
tsechov <tsechov@gmail.com>
2014-08-19 17:36:49 +0200

    #2 adding warehouse model and controller

---
+++ app/controllers/Warehouses.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import play.api.mvc.Action
+import models.AssetSupport.IdType
+
+import play.api.mvc.BodyParsers.parse
+import models.customer.{WarehouseCreate, WarehouseUpdate, Warehouse}
+
+
+object Warehouses extends CrudController {
+  override type MODEL = Warehouse
+  override type UPDATEMODEL = WarehouseUpdate
+  override type CREATEMODEL = WarehouseCreate
+
+
+  def create = Action.async(parse.json) {
+    request =>
+      super.create(request.body, id => controllers.routes.Warehouses.getById(id))
+  }
+
+  def update(id: IdType) = Action.async(parse.json) {
+    request =>
+      super.update(id, request.body)
+  }
+
+  def delete(id: String) = Action.async {
+    super.delete(id)
+  }
+
+}
--- app/models/customer/warehouse.scala
+++ app/models/customer/warehouse.scala
@@ -3,14 +3,58 @@ package models.customer
 import models.common.Address
 import models.AssetSupport._
 import play.api.libs.json.Json
+import services.CollectionName
+import models.AssetSupport.IdType
+import org.joda.time.DateTime
+import models.{AssetCreate, AssetUpdate}
 
 case class Warehouse(
+                      id: IdType,
+                      createdAt: DateTime,
+                      lastModifiedAt: DateTime,
+                      active: Boolean,
+                      description: String,
                       name: String,
                       address: Address,
                       status: Boolean,
                       url: UrlType
-                      )
+                      ) extends AssetUpdate[WarehouseUpdate] {
+  override def fillup(lastModifiedAt: DateTime): WarehouseUpdate = WarehouseUpdate(lastModifiedAt, active, description, name, address, status, url)
+}
 
 object Warehouse {
   implicit val format = Json.format[Warehouse]
+  implicit val collectionName = new CollectionName[Warehouse] {
+    override def get: String = "warehouses"
+  }
+}
+
+case class WarehouseUpdate(lastModifiedAt: DateTime,
+                           active: Boolean,
+                           description: String,
+                           name: String,
+                           address: Address,
+                           status: Boolean,
+                           url: UrlType)
+
+object WarehouseUpdate {
+
+  implicit val format = Json.format[WarehouseUpdate]
+  implicit val collectionName = new CollectionName[WarehouseUpdate] {
+    override def get: String = Warehouse.collectionName.get
+  }
+}
+
+case class WarehouseCreate(active: Boolean,
+                           description: String,
+                           name: String,
+                           address: Address,
+                           status: Boolean,
+                           url: UrlType) extends AssetCreate[Warehouse] {
+  override def fillup(id: IdType, createdAt: DateTime, lastModifiedAt: DateTime) = Warehouse(id, createdAt, lastModifiedAt, active, description, name, address, status, url)
+}
+
+object WarehouseCreate {
+
+  implicit val reads = Json.reads[WarehouseCreate]
 }
