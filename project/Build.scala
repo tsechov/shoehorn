e57db01327f6b664fdeeb2bc2b729513cf5bba10
tsechov <tsechov@gmail.com>
2014-09-03 07:27:41 +0200

    version bump override

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,13 @@ import sbt.File
 import sbt.Keys._
 import play.Project._
 import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.Version.Bump
 import scala.Some
 
 object ApplicationBuild extends Build {
   releaseSettings
-  
+
 
   val appName = "shoehorn"
   val appVersion = "NA"
@@ -24,11 +26,18 @@ object ApplicationBuild extends Build {
   )
 
 
+  def bumpType = sys.props.get("versionBump") match {
+    case Some("major") => Bump.Major
+    case Some("minor") => Bump.Minor
+    case _ => Bump.Next
+  }
+
   val main = play.Project(appName, appVersion, appDependencies, settings = Defaults.defaultSettings ++ playScalaSettings ++ releaseSettings).settings(
     sources in(Compile, doc) := Seq.empty,
     publishTo := Some(Resolver.file("file", new File(target.value.absolutePath + "/publish"))),
     version := (version in ThisBuild).value,
 
+    versionBump := bumpType,
 
     sourceGenerators in Compile <+= Def.task {
       val file = (sourceManaged in Compile).value / "release" / "CurrentVersion.scala"
