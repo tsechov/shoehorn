555e27be7929610e3eff27ce4a698755d1f33684
tsechov <tsechov@gmail.com>
2014-10-09 07:38:34 +0200

    fixing report test with test images

--- test/models/order/OrderReportTest.scala
+++ test/models/order/OrderReportTest.scala
@@ -17,9 +17,9 @@ class OrderReportTest extends Specification {
       val customer = CustomerReport("name", address, shippingName, shippingAddress, "adoszam123", "bankszamlaszamjool")
       val agent = AgentReport("neve", "payment", "phonenumber123", email = "foo@bar.com")
       val sortiment1 = List(SortimentItem(18, 2), SortimentItem(24, 1), SortimentItem(34, 5))
-      val items1 = ProductReport("1011-22432", "https://dl.dropboxusercontent.com/u/14779005/szamos/szamos-frontend/shoes/1011-22432-full.jpg", sortiment1)
+      val items1 = ProductReport("1011-22432", "https://raw.githubusercontent.com/tsechov/shoehorn/68ea2560e9bcc703703565bf728e5a3a93ececd5/test/resources/1195-21709-thumb.jpg", sortiment1)
       val sortiment2 = List(SortimentItem(19, 2), SortimentItem(24, 1), SortimentItem(34, 5))
-      val items2 = ProductReport("1127-22182", "https://dl.dropboxusercontent.com/u/14779005/szamos/szamos-frontend/shoes/1127-22182-full.jpg", sortiment2)
+      val items2 = ProductReport("1127-22182", "https://raw.githubusercontent.com/tsechov/shoehorn/68ea2560e9bcc703703565bf728e5a3a93ececd5/test/resources/1222-43633-thumb.jpg", sortiment2)
       val order = OrderReport("idjool", "ordernumber", new DateTime, Some(("d1", new DateTime)), Some(("d3", new DateTime)), None, None, None, customer, agent, List(items1, items2), 16)
 
       val xml = ToXml.get(order)
