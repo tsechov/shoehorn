cbbbc884c572033bf6bbd6e91ffa1d71ce33cdfc
tsechov <tsechov@gmail.com>
2014-11-10 19:53:22 +0100

    echo -e

--- release-wercker.sh
+++ release-wercker.sh
@@ -3,8 +3,8 @@ if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
 
     git checkout master
     mkdir ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
-    echo $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-    echo $GITHUB_KEY_PUBLIC > ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa.pub
+    echo -e $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+    echo -e $GITHUB_KEY_PUBLIC > ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa.pub
     ls -al ~/.ssh
     cat ~/.ssh/id_rsa
     ./github-release-latest.sh
