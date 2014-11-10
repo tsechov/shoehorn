678763cf1c61faff226799a3df2014575ddc2652
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-11-10 12:36:08 +0100

    adding github.com's ssh key to known_hosts

--- release-wercker.sh
+++ release-wercker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
+
     git checkout master
+    ssh-keyscan -H github.com >> ~/.ssh/known_hosts
     ./github-release-latest.sh
 else
     echo "not on perform-release target, not releasing"
