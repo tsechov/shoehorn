759add2ee75d218c3798b37fb6ba11c368ad04e7
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-11-10 14:39:43 +0100

    adding github.com's ssh key to known_hosts again and again rsa

--- release-wercker.sh
+++ release-wercker.sh
@@ -3,7 +3,7 @@ if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
 
     git checkout master
     mkdir ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
-    echo $GITHUB_KEY_PRIVATE > ~/.ssh/identity && chmod 600 ~/.ssh/identity
+    echo $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
     ./github-release-latest.sh
 else
