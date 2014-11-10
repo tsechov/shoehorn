b56ca715d76cb2ee0342ba35d56e5e6f5be5faf1
tsechov <tsechov@gmail.com>
2014-11-10 20:02:58 +0100

    remove ssh debug

--- release-wercker.sh
+++ release-wercker.sh
@@ -5,8 +5,8 @@ if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
     mkdir ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
     echo -e $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
     echo -e $GITHUB_KEY_PUBLIC > ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa.pub
-    ls -al ~/.ssh
-    cat ~/.ssh/id_rsa
+    git config user.email "tsechov@gmail.com"
+    git config user.name "tsechov"
     ./github-release-latest.sh
 else
     echo "not on perform-release target, not releasing"
