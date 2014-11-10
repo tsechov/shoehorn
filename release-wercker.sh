08bfb3aa40adcd4d5d590240c9b26d1a978d8549
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-11-10 18:23:20 +0100

    ssh debug

--- release-wercker.sh
+++ release-wercker.sh
@@ -5,6 +5,7 @@ if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
     mkdir ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
     echo $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
     echo $GITHUB_KEY_PUBLIC > ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa.pub
+    ls -al ~/.ssh
     ./github-release-latest.sh
 else
     echo "not on perform-release target, not releasing"
