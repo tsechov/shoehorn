407c5e0fd1e1e08950ddaa3fae2e9e9013dd33b5
Akos Mocsanyi <extern.amocsanyi@porsche.hu>
2014-11-10 17:44:40 +0100

    adding github public key

--- release-wercker.sh
+++ release-wercker.sh
@@ -4,7 +4,7 @@ if [ "$WERCKER_DEPLOYTARGET_NAME" == "perform-release" ]; then
     git checkout master
     mkdir ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
     echo $GITHUB_KEY_PRIVATE > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-
+    echo $GITHUB_KEY_PUBLIC > ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa.pub
     ./github-release-latest.sh
 else
     echo "not on perform-release target, not releasing"
