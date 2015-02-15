df743fa9c97773e421635bfadeeead8e788d8ed6
tsechov <tsechov@gmail.com>
2015-02-15 23:04:12 +0100

    calculation fix

--- app/services/mongo/MongoUpdate.scala
+++ app/services/mongo/MongoUpdate.scala
@@ -1,19 +1,25 @@
 package services.mongo
 
+import models.DateFormatSupport
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.obj
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import reactivemongo.core.commands.LastError
+import play.api.libs.json._
 
 import scala.concurrent.Future
 
 /**
  * @author tsechov
  */
-trait MongoUpdate {
+trait MongoUpdate extends DateFormatSupport {
   self: MongoCollection =>
   def update(selector: JsObject, data: JsValue): Future[LastError] = {
-    collection.update(selector, obj("$set" -> data))
+    val transformer = (__ \ '_id).json.prune
+    val woId = data.transform(transformer).get
+    dateFormat
+    val jsonTransformer = (__ \ 'lastModifiedAt).json.put(JsNumber(456))
+    collection.update(selector, obj("$set" -> woId))
   }
 }
